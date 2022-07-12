474403ab06bc86e7556ddbf0369c3a4cb7ed931d
pierre <pierre>
2022-07-12 21:28:21 +0200

    display progress in the test program

--- leaprs_testing/src/main.rs
+++ leaprs_testing/src/main.rs
@@ -1,75 +1,106 @@
+use std::time::Duration;
+
 use leaprs::*;
 use throbber::Throbber;
 
 fn main() {
-    let mut throbber = Throbber::new();
-    throbber.start_with_msg("Connecting to the service...".to_string());
     let mut connection = Connection::create().expect("Failed to create connection");
     connection.open().expect("Failed to open the connection");
 
-    connection.wait_for(|e| match e {
-        Event::ConnectionEvent(_) => true,
-        _ => false,
+    connection.wait_for("Connecting to the service...".to_string(), |e| match e {
+        Event::ConnectionEvent(_) => Msg::Success("Connected".to_string()),
+        _ => Msg::None,
     });
 
-    throbber.success("Connected".to_string());
-
-    throbber.start_with_msg("Waiting for a device...".to_string());
-
-    connection.wait_for(|e| match e {
-        Event::DeviceEvent(_) => true,
-        _ => false,
+    connection.wait_for("Waiting for a device...".to_string(), |e| match e {
+        Event::DeviceEvent(_) => Msg::Success("Got a device".to_string()),
+        _ => Msg::None,
     });
 
-    throbber.success("Got a device".to_string());
-
-    throbber.start_with_msg("Waiting for a hand...".to_string());
-
-    connection.wait_for(|e| match e {
-        Event::TrakingEvent(e) => e.get_hands().len() > 0,
-        _ => false,
+    connection.wait_for("Waiting for a hand...".to_string(), |e| match e {
+        Event::TrakingEvent(e) => {
+            if e.get_hands().len() > 0 {
+                Msg::Success("Got a hand".to_string())
+            } else {
+                Msg::None
+            }
+        }
+        _ => Msg::None,
     });
 
-    throbber.success("Got a hand".to_string());
-
-    throbber.start_with_msg("Close the hand".to_string());
-
-    connection.wait_for(|e| match e {
-        Event::TrakingEvent(e) => e.get_hands().iter().any(|hand| hand.grab_strength >= 0.999),
-        _ => false,
+    connection.wait_for("Close the hand".to_string(), |e| match e {
+        Event::TrakingEvent(e) => {
+            if let Some(hand) = e.get_hands().first() {
+                let grab_strength = hand.grab_strength;
+                if grab_strength >= 1.0 {
+                    Msg::Success("The hand is closed".to_string())
+                } else {
+                    Msg::Progress(format!("Close the hand {:.0}%", grab_strength * 100.0))
+                }
+            } else {
+                Msg::Progress("Close the hand".to_string())
+            }
+        }
+        _ => Msg::None,
     });
 
-    throbber.success("The hand is closed".to_string());
-
-    throbber.start_with_msg("Open the hand".to_string());
-
-    connection.wait_for(|e| match e {
-        Event::TrakingEvent(e) => e.get_hands().iter().any(|hand| hand.grab_strength <= 0.001),
-        _ => false,
+    connection.wait_for("Open the hand".to_string(), |e| match e {
+        Event::TrakingEvent(e) => {
+            if let Some(hand) = e.get_hands().first() {
+                let ungrab_strength = 1.0 - hand.grab_strength;
+                if ungrab_strength >= 0.999 {
+                    Msg::Success("The hand is opened".to_string())
+                } else {
+                    Msg::Progress(format!("Open the hand {:.0}%", ungrab_strength * 100.0))
+                }
+            } else {
+                Msg::Progress("Open the hand".to_string())
+            }
+        }
+        _ => Msg::None,
     });
+}
 
-    throbber.success("The hand is opened".to_string());
-
-    throbber.end();
+pub enum Msg {
+    None,
+    Success(String),
+    Progress(String),
+    Failure(String),
 }
 
 trait WaitFor {
-    fn wait_for<F>(&mut self, condition: F)
+    fn wait_for<F>(&mut self, message: String, condition: F)
     where
-        F: Fn(&Event) -> bool;
+        F: Fn(&Event) -> Msg;
 }
 
 impl WaitFor for Connection {
-    fn wait_for<F>(&mut self, condition: F)
+    fn wait_for<F>(&mut self, message: String, condition: F)
     where
-        F: Fn(&Event) -> bool,
+        F: Fn(&Event) -> Msg,
     {
+        let mut throbber = Throbber::new().interval(Duration::from_millis(100));
+
+        throbber.start_with_msg(message);
+
         loop {
             if let Ok(message) = self.poll(100) {
-                if condition(&message.event()) {
-                    break;
+                match condition(&message.event()) {
+                    Msg::None => {}
+                    Msg::Success(message) => {
+                        throbber.success(message);
+                        break;
+                    }
+                    Msg::Progress(message) => {
+                        throbber.change_message(message);
+                    }
+                    Msg::Failure(message) => {
+                        throbber.fail(message);
+                        break;
+                    }
                 }
             }
         }
+        throbber.end();
     }
 }
