ec9076c6d2273f5fc575aba29388b5bbc900a99c
pierre <pierre>
2022-07-18 16:02:11 +0200

    revert hacky cov setup

--- test.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item -Confirm:$false -Recurse 'coverage'
-cargo test -- --show-output
-llvm-profdata merge -sparse coverage/leaprs-*.profraw -o coverage/leaprs.profdata
-llvm-cov export target\debug\deps\leaprs-7372d08029cc2e95.exe -instr-profile=".\coverage\leaprs.profdata" --ignore-filename-regex='\\.cargo\\registry' --Xdemangler=rustfilt -format=lcov | rustfilt | Out-File -Encoding UTF8 -FilePath .\coverage\lcov.info
