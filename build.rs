2302e0e594066601f3cbe357389a01c2a81141a1
Stephen Buchanan <webtvmaster@gmail.com>
2024-06-18 17:13:39 +0200

    new format for rustc v1.77+

--- build.rs
+++ build.rs
@@ -12,7 +12,7 @@ const DEFAULT_LEAPSDK_LIB_PATH: &str = r"/usr/share/doc/ultraleap-hand-tracking-
 
 fn main() {
     // Find Leap SDK
-    println!(r"cargo:rerun-if-env-changed=LEAPSDK_LIB_PATH");
+    println!(r"cargo::rerun-if-env-changed=LEAPSDK_LIB_PATH");
 
     let leapsdk_path =
         env::var("LEAPSDK_LIB_PATH").unwrap_or_else(|_| DEFAULT_LEAPSDK_LIB_PATH.to_string());
@@ -20,14 +20,14 @@ fn main() {
     let leapsdk_path = PathBuf::from(leapsdk_path);
 
     if !leapsdk_path.is_dir() {
-        println!("cargo:warning=Could not find LeapSDK at the location {}. Install it from https://developer.leapmotion.com/tracking-software-download or set its location with the environment variable LEAPSDK_LIB_PATH.", leapsdk_path.display());
+        println!("cargo::warning=Could not find LeapSDK at the location {}. Install it from https://developer.leapmotion.com/tracking-software-download or set its location with the environment variable LEAPSDK_LIB_PATH.", leapsdk_path.display());
     } else {
         let path_str = leapsdk_path
             .to_str()
             .unwrap_or_else(|| panic!("{} is not a valid path.", leapsdk_path.display()));
 
         // Link to LeapC.lib
-        println!(r"cargo:rustc-link-search={}", path_str);
-        println!(r"cargo:rustc-link-lib=LeapC");
+        println!(r"cargo::rustc-link-search={}", path_str);
+        println!(r"cargo::rustc-link-lib=LeapC");
     }
 }
